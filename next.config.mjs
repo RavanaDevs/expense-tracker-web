2ec4e1749ccc703a1ede33b82402781d6d0d38f4
Kavindu Sanjula <123143684+RavanaDevs@users.noreply.github.com>
2024-11-19 10:21:01 +0530

    Install axios and add remort origin

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://170.187.237.26:5555/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
