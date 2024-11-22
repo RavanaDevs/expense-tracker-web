266c5d6b5d34f2d67293d23d04891cc9c9039b83
Kavindu Sanjula <123143684+RavanaDevs@users.noreply.github.com>
2024-11-22 19:59:34 +0530

    Get all settings

--- app/api/settings/route.ts
+++ app/api/settings/route.ts
@@ -4,16 +4,7 @@ import { settingsSchema } from "@/validators/settingsSchemaValidator";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
-const getOrCreate = async (userId: string): Promise<ISettings> => {
-  await connectToDatabase();
-  const settings = await Settings.findOne({ user: userId });
-  if (settings) return settings;
-
-  const newSettings = new Settings({ user: userId });
-  return await newSettings.save();
-};
-
-export async function PUT(req: NextRequest) {
+export async function GET() {
   const { userId } = await auth();
   if (!userId) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
@@ -21,28 +12,10 @@ export async function PUT(req: NextRequest) {
 
   try {
     await connectToDatabase();
-    const validationData = settingsSchema.parse(await req.json());
-    if (!validationData)
-      return NextResponse.json(
-        { message: "Error in request body" },
-        { status: 400 }
-      );
-
-    const settings = await getOrCreate(userId);
-
-    if (
-      validationData.quickAmountSettings &&
-      validationData.quickAmountSettings[0]
-    ) {
-      settings.quickAmountSettings = validationData.quickAmountSettings;
-      const updated = await settings.save();
-
-      console.log(updated);
-    }
-
+    const settings = await Settings.findOne({ user: userId });
     return NextResponse.json(settings);
   } catch (err) {
-    console.log("Error while updating settings. -> ", err);
+    console.log("Error while retrieving currency settings. -> ", err);
     return NextResponse.json({ message: "Error" });
   }
 }
