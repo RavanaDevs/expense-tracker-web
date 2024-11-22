2418be34ffbfd6e4cab1528c4ba51705df8e5ec0
Kavindu Sanjula <123143684+RavanaDevs@users.noreply.github.com>
2024-11-22 21:03:27 +0530

    Centralized error handler

--- app/api/settings/categories/route.ts
+++ app/api/settings/categories/route.ts
@@ -3,40 +3,31 @@ import Settings from "@/models/settingsModel";
 import { categorySettinsSchema } from "@/validators/settingsSchemaValidator";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
+import { withErrorHandler } from "@/utils/errorHandler";
 
-export async function GET(req: NextRequest) {
-  const { userId } = await auth();
-  if (!userId) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  }
+export const GET = withErrorHandler(async (req: NextRequest) => {
+    const { userId } = await auth();
+    if (!userId) {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
 
-  try {
     await connectToDatabase();
     const settings = await Settings.findOne({ user: userId });
     return NextResponse.json(settings?.categorySettings || []);
-  } catch (err) {
-    console.log("Error while retrieving category settings. -> ", err);
-    return NextResponse.json({ message: "Error" });
-  }
-}
+});
 
-export async function PUT(req: NextRequest) {
-  const { userId } = await auth();
-  if (!userId) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  }
+export const PUT = withErrorHandler(async (req: NextRequest) => {
+    const { userId } = await auth();
+    if (!userId) {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
 
-  try {
     await connectToDatabase();
     const validationData = categorySettinsSchema.parse(await req.json());
     const settings = await Settings.findOneAndUpdate(
-      { user: userId },
-      { categorySettings: validationData },
-      { new: true, upsert: true }
+        { user: userId },
+        { categorySettings: validationData },
+        { new: true, upsert: true }
     );
     return NextResponse.json(settings.categorySettings);
-  } catch (err) {
-    console.log("Error while updating category settings. -> ", err);
-    return NextResponse.json({ message: "Error" });
-  }
-}
+});
--- app/api/settings/currency/route.ts
+++ app/api/settings/currency/route.ts
@@ -3,40 +3,31 @@ import Settings from "@/models/settingsModel";
 import { currencySettingsSchema } from "@/validators/settingsSchemaValidator";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
+import { withErrorHandler } from "@/utils/errorHandler";
 
-export async function GET(req: NextRequest) {
-  const { userId } = await auth();
-  if (!userId) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  }
+export const GET = withErrorHandler(async (req: NextRequest) => {
+    const { userId } = await auth();
+    if (!userId) {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
 
-  try {
     await connectToDatabase();
     const settings = await Settings.findOne({ user: userId });
     return NextResponse.json(settings?.currencySettings || {});
-  } catch (err) {
-    console.log("Error while retrieving currency settings. -> ", err);
-    return NextResponse.json({ message: "Error" });
-  }
-}
+});
 
-export async function PUT(req: NextRequest) {
-  const { userId } = await auth();
-  if (!userId) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  }
+export const PUT = withErrorHandler(async (req: NextRequest) => {
+    const { userId } = await auth();
+    if (!userId) {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
 
-  try {
     await connectToDatabase();
     const validationData = currencySettingsSchema.parse(await req.json());
     const settings = await Settings.findOneAndUpdate(
-      { user: userId },
-      { currencySettings: validationData },
-      { new: true, upsert: true }
+        { user: userId },
+        { currencySettings: validationData },
+        { new: true, upsert: true }
     );
     return NextResponse.json(settings.currencySettings);
-  } catch (err) {
-    console.log("Error while updating currency settings. -> ", err);
-    return NextResponse.json({ message: "Error" });
-  }
-}
+});
